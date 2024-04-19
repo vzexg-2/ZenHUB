32d59ce20594d1a7857b54def3f4c3fd6640a4fe
SunshineJ <155290329+vzexg-2@users.noreply.github.com>
2024-04-19 23:22:01 +0700

    Update ZenHuB.lua

--- ZenHuB/Ow/ow/ZenHuB.lua
+++ ZenHuB/Ow/ow/ZenHuB.lua
@@ -3,10 +3,11 @@
  |_  /___ _ _ / __| ___ __  \ \ / /__ /
   / // -_) ' \\__ \/ -_) _|  \ V / |_ \
  /___\___|_||_|___/\___\__|   \_/ |___/
-    ! Secured By ZenSec ( Alpha v0.3.2 ) Much love, Sunshine
+    ! Secured By ZenSec ( Alpha v0.3.2 ) Much love, Sunshine.
 ]]
 
 
+local z1='[[ Secured by ZenSec ]]'
 local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 function orEUpqvHbPDQmEldTtEBZpNqDnlnZNZVcEakdxayiJ(data) m=string.sub(data, 0, 50) data=data:gsub(m,'')
 data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
